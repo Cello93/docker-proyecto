8f1852e8f0ea5566f70bb6c5e6524f238f012539
Cello93 <alee93alvarez@gmail.com>
2024-01-30 18:03:26 -0300

    se agrega cambios

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -12,7 +12,7 @@ export class TodoService {
     { id: 4, description: 'Piedra del Tiempo', done: false },
     { id: 5, description: 'Piedra desde el contenedor', done: true },
     { id: 6, description: 'Piedra desde el as', done: true },
-    { id: 6, description: 'Piedra desde el nuevo', done: true },
+    { id: 6, description: 'Piedra desde el s', done: true },
   ];
 
   get totalTodos() {
